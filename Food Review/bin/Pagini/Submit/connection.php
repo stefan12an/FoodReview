3d3bb4a36647d7a6f1b176efef82cb237936e158
stefan12an <68771082+stefan12an@users.noreply.github.com>
2020-07-25 11:26:27 +0300

    Add files via upload

---
+++ Food Review/bin/Pagini/Submit/connection.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "1904stef";
+$dbName = "login";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, 'login', '3308');
+?>
